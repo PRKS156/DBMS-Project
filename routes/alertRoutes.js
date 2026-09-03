4f131c338c2cca38b8fc81d80eda7dd29c800c83
PrakharG-spec <prakhargawali15@gmail.com>
2026-09-03 12:20:41 +0530

    Register missing alert status route

--- routes/alertRoutes.js
+++ routes/alertRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const alertController = require('../controllers/alertController');
-
 router.post('/trigger', alertController.triggerAlert);
 router.get('/doctor/:doctorId', alertController.getDoctorAlerts);
+router.get('/:id/status', alertController.getAlertStatus);
 router.patch('/:id/acknowledge', alertController.acknowledgeAlert);
-
 module.exports = router;
